e785bb8f0f089d1a369956d713699fbaffa8d83d
Jingtao-Qu <757071279@qq.com>
2024-10-07 20:19:26 +0800

    Create Step2-mapping_positon.pl

---
+++ Step2-mapping_positon.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use Getopt::Long;
+use PerlIO::gzip;
+my ($in_vcf, $out_vcf);
+
+GetOptions(
+	'i:s' => \$in_vcf,
+	'o:s' => \$out_vcf,
+) or die $!;
+
+open IN,"<:gzip","$in_vcf" || die $!;
+open OUT,">:gzip","$out_vcf" || die $!;
+while (<IN>) {
+	if (/^#/) {
+		print OUT;
+	}else{
+		my @vcf = split/\s+/,$_,3;
+		my @name = split/:/,$vcf[0];
+		print OUT join("\t",($name[0],$name[1]+$vcf[1],$vcf[2]));
+	}
+}
+close OUT;
